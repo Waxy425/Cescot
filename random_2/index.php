14e47360a77f0f1d8c09096b952661e5bbe61ab0
Dennis <dennisrexhepi@gmail.com>
2025-11-26 16:24:15 +0100

    ci siamo

--- random_2/index.php
+++ random_2/index.php
@@ -8,28 +8,34 @@
 </head>
 <body>
     <h1>Contenuti a caso</h1>
-     <?php div_generator();
+     <?php div_generator(rand(10,30));
      ?>
 
 </body>
 </html>
 
 <?php
-$numero_div = rand(10,30);
-function div_generator($numero_div);
+
+function div_generator($numero_div){
     $i=0;
-    if ($numero_div %2 === true){
+    if ($numero_div %2 == true){
         $n=$numero_div/2;
         while ($i<$n):
             echo'<div class="dispari">questo div è dispari</div>';
             echo'<div class="pari">questo div è pari</div>';
+            $i++;
+        endwhile;
     } else {
-        $n=$numero_div-1;
-        while ($i<$n):
+        $m=$numero_div-1;
+        while ($i<$m):
+            
             echo'<div class="dispari">questo div è dispari</div>';
             echo'<div class="pari">questo div è pari</div>';
-        echo'<div class="dispari">questo div è dispari</div>';
-    }
-
+            $i++;
+        endwhile;
+        echo'<div class="dispari">questo div è dispari</div>'; 
+        }
+    
+}
 
     
