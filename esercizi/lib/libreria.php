c5f94b6af6bb1e0e06f8ef604d1caefbd2d796e5
Dennis <dennisrexhepi@gmail.com>
2025-12-12 12:39:57 +0100

    aggiunti commenti copilot

--- esercizi/lib/libreria.php
+++ esercizi/lib/libreria.php
@@ -1,20 +1,27 @@
 <?php
+// File di libreria con generatori di <div>
+
 function div_generatore_dispari_pari($pari_dispari_div){
-      $p="pari";
-      $d="dispari";
-      $k=0;
+      // $pari_dispari_div: numero di coppie di div da generare
+      $p="pari"; // nome classe per i div pari (non usato direttamente)
+      $d="dispari"; // nome classe per i div dispari (non usato direttamente)
+      $k=0; // contatore delle iterazioni
         while($k<$pari_dispari_div):
-            $k++;
+            $k++; // incremento contatore
             
+            // Stampo prima un div "pari" poi un div "dispari"
             echo'<div class="pari">questo div è pari</div>';
             echo'<div class="dispari">questo div è dispari</div>';
         endwhile;
     };
 
 function div_generatore_random($min, $max){
-    $i=0;
-    $numero_div = rand($min,$max);
+    // Genera un numero casuale di div compreso tra $min e $max.
+    // Se il numero è dispari stampa una sequenza alternata pari/dispari adeguata.
+    $i=0; // contatore delle coppie stampate
+    $numero_div = rand($min,$max); // numero totale di div da generare
     if ($numero_div %2 == true){
+        // numero dispari: calcolo metà (float) e genero coppie fino a $n
         $n=$numero_div/2;
         while ($i<$n):
             echo'<div class="dispari">questo div è dispari</div>';
@@ -22,13 +29,15 @@ function div_generatore_random($min, $max){
             $i++;
         endwhile;
     } else {
+        // numero pari: genero coppie fino a $m (numero_div - 1) poi un ultimo div dispari
         $m=$numero_div-1;
         while ($i<$m):
-            
+            // Stampo coppie dispari/pari
             echo'<div class="dispari">questo div è dispari</div>';
             echo'<div class="pari">questo div è pari</div>';
             $i++;
         endwhile;
+        // Stampo l'ultimo div dispari rimanente
         echo'<div class="dispari">questo div è dispari</div>'; 
         }
     
@@ -36,43 +45,8 @@ function div_generatore_random($min, $max){
 
 
     function div_generatore($numero_div, $class="", $id="", $contenuto=""){
+      // Genera $numero_div di <div> con attributi opzionali: class, id e contenuto
       for($i=0;$i<$numero_div;$i++) {
         echo"<div class='$class' id='$id'>$contenuto</div>";
     }
     };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-?>
