265152d550b9b5020b147cdddde6b54ac61fcef9
Alex <ciottialex.06@gmail.com>
2025-12-17 12:50:06 +0100

    tolto "visualizza a tutto schermo"

--- esercizi/groups/index.php
+++ esercizi/groups/index.php
@@ -21,7 +21,6 @@
         <button id="terzogiallo" onclick="terzo_giallo()">terzo giallo</button>
         <button id="tuttigiallo" onclick="tutti_giallo()">tutti giallo</button>
         <button onclick="reset()">reset</button>
-        <button onclick="sorry()">Visualizza a tutto schermo</button>
         <button onclick="sorry()">Divertiti</button>
         <img id="myImage" 
         src="https://info.grantmcgregor.co.uk/hubfs/we-are-sorry-microsoft-blue-screen-sad-smiley.jpg" 
