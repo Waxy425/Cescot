07921c76bb6a9d3e5b2a18173b2f53e04229bf75
Alex <ciottialex.06@gmail.com>
2026-01-16 12:47:43 +0100

    Add region filtering functionality to clienti.php and regioni.php

--- esercizi/database/prenotazioni/clienti.php
+++ esercizi/database/prenotazioni/clienti.php
@@ -15,11 +15,47 @@
 
     $connessione = mysqli_connect($host, $user, $pass, $dbname);
 
-    $query = "SELECT DISTINCT ID_cliente, nome, cognome, citta.citta, regioni.regione, regioni.area_geografica FROM regioni INNER JOIN citta ON regioni.ID_regione = citta.regione INNER JOIN clienti ON citta.ID_citta = clienti.citta";
+    $query = "SELECT DISTINCT ID_cliente, nome, cognome, citta.citta, regioni.regione, regioni.area_geografica 
+    FROM regioni INNER JOIN citta ON regioni.ID_regione = citta.regione 
+    INNER JOIN clienti ON citta.ID_citta = clienti.citta";
+    if(isset($_GET["regione_scelta"])) {
+        $lettore = $_GET["regione_scelta"];
+        $query .= " WHERE regioni.regione = '$lettore'";
+    }
     $risultato = mysqli_query($connessione, $query);
+
 ?>
 
 <body>
+    
+
+    <form action="" method="get">
+        <select name="regione_scelta" id="regioni">
+            <option value="Abruzzo">Abruzzo</option>
+            <option value="Basilicata">Basilicata</option>
+            <option value="Calabria">Calabria</option>
+            <option value="Campania">Campania</option>
+            <option value="Emilia-Romagna">Emilia-Romagna</option>
+            <option value="Friuli-Venezia Giulia">Friuli-Venezia Giulia</option>
+            <option value="Lazio">Lazio</option>
+            <option value="Liguria">Liguria</option>
+            <option value="Lombardia">Lombardia</option>
+            <option value="Marche">Marche</option>
+            <option value="Molise">Molise</option>
+            <option value="Piemonte">Piemonte</option>
+            <option value="Puglia">Puglia</option>
+            <option value="Sardegna">Sardegna</option>
+            <option value="Sicilia">Sicilia</option>
+            <option value="Toscana">Toscana</option>
+            <option value="Trentino-Alto Adige">Trentino-Alto Adige</option>
+            <option value="Umbria">Umbria</option>
+            <option value="Valle d'Aosta">Valle d'Aosta</option>
+            <option value="Veneto">Veneto</option>
+        </select>
+        <input type="submit" value="cerca">Invia</input>
+    </form>
+
+
     <h1>Clienti</h1>
     <?php
         include '../../lib/libreria.php';
--- esercizi/database/prenotazioni/regioni.php
+++ esercizi/database/prenotazioni/regioni.php
@@ -12,16 +12,30 @@
     $user = getenv("DB_USER");
     $pass = getenv("DB_PASS");
     $dbname = getenv("DB_NAME");
+    $filtro = isset($_GET['cerca_regione']) ? $_GET['cerca_regione'] : '';
 
     $connessione = mysqli_connect($host, $user, $pass, $dbname);
+    $reg = $_GET['cerca_regione'];
 
     $query = "SELECT DISTINCT regioni.regione, ID_prenotazione, COUNT(*) AS numero_prenotazioni,
     ROUND(SUM(prenotazioni.importo), 2) AS totale_importo,
     ROUND(SUM(prenotazioni.importo - prenotazioni.caparra), 2) AS totale_saldo FROM regioni
     INNER JOIN citta ON regioni.ID_regione = citta.regione
     INNER JOIN clienti ON citta.ID_citta = clienti.citta
-    INNER JOIN prenotazioni ON clienti.ID_cliente = prenotazioni.cliente
-    GROUP BY regioni.regione";
+    INNER JOIN prenotazioni ON clienti.ID_cliente = prenotazioni.cliente";
+
+    
+    $query .= " WHERE regioni.regione LIKE '%$reg%'";
+    
+    $query .= " GROUP BY regioni.regione";
+
+
+    /*
+    WHERE regioni.regione LIKE ?
+    GROUP BY regioni.regione
+    */
+
+    //HAVING regione = '" . $cerca_regione . "'
 
     //prenotazioni.ID_prenotazione, importo, caparra FROM regioni INNER JOIN citta ON regioni.ID_regione = citta.regione INNER JOIN clienti ON citta.ID_citta = clienti.citta INNER JOIN prenotazioni ON clienti.ID_cliente = prenotazioni.cliente;
 
@@ -29,6 +43,13 @@
 ?>
 
 <body>
+    <form method="get">
+        <label for="cerca_regione">inserisci regione</label>
+        <input type="text" id="cerca_regione" name="cerca_regione" placeholder="scrivi qui"></input>
+        <input type="submit" value="cerca"></input>
+    </form>
+
+
     <h1>Regioni</h1>
     <?php
         include '../../lib/libreria.php';
@@ -37,6 +58,7 @@
             $regione             = $tutto['regione'];
             $importo             = $tutto['totale_importo'];
             $saldo               = $tutto['totale_saldo'];
+            
             
             
             div_generatore(1, "", "", 
@@ -48,7 +70,12 @@
             </p>
             ");
         }
+
+
+        /*Aggiungere un campo di testo che consenta all'utente di inserirre il nome di una regione o parte del nome di una regione, se l'utente ha compilato il valore, mostrare solamente i div relativi alle regioni che corrispondono al valore inserito dall'utente, altrimenti mostrare tutte le regioni*/
     ?>
+
+
 
     
 </body>
